be3cf19afc726f23812213edf18f558ec99f553f
Derek <berkagmp@gmail.com>
2020-07-01 05:56:14 +1200

    Fix the Router

--- resources/js/Router.js
+++ resources/js/Router.js
@@ -13,14 +13,11 @@ import UserList from "./components/UserList";
 const Main = props => (
     <Switch>
         {/*User might LogIn*/}
-        <Route exact path="/" component={UserList} />
+        <PrivateRoute exact path="/" component={UserList} />
 
         {/*User will LogIn*/}
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
-
-        {/* User is LoggedIn*/}
-        <PrivateRoute path="/" component={UserList} />
 
         {/*Page Not Found*/}
         <Route component={NotFound} />
